bb48c9c36ad2cf32ec23dd3db3ba807ff144a305
Alexandre-Prz <alexan2000@live.fr>
2022-10-10 17:20:13 +0200

    Affichage grille fonctionnel

--- LinesDrawingExample.java
+++ LinesDrawingExample.java
@@ -13,21 +13,21 @@ import javax.swing.SwingUtilities;
  */
 public class LinesDrawingExample extends JFrame {
     private int i=0;
-    private int widthSize=480,heightSize=200;
+    private int widthSize=480,heightSize=480;
+    private int offset=50;
     private int nRows=10,nColumns=10;
     public LinesDrawingExample() {
         super("Lines Drawing Demo");
  
         setSize(widthSize, heightSize);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocation(new Point(0, 0));
         setLocationRelativeTo(null);
         this.gameLoop.start();
         
     }
     private Thread gameLoop = new Thread(() -> {
         while (true) {
-            this.i++;
+            //this.i++;
             this.repaint();
             try {
                 Thread.sleep(1000/60);
@@ -37,19 +37,32 @@ public class LinesDrawingExample extends JFrame {
     });
     void drawLines(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
- 
-        //g2d.drawLine(120+i, 50-i, 360+i, 50-i);
-        for (int i =0;i<=nRows;i++){
+        int x1,x2,y1,y2;
+        int newWidth=widthSize-2*offset,newHeight=heightSize-2*offset;// On définit newWidth et newHeight qui sont est la nouvelle largeur et hauteur du carré sur lequel on va vraiment dessiner le terrain(sans les offset)
+    
+
+        for (int i =0;i<=nRows+1;i++){
+            x1=offset;
+            y1=offset+newHeight*i/(nRows+1);
+            x2=offset+newWidth;
+            y2=offset+newHeight*i/(nRows+1);
+            g2d.drawLine(x1,y1,x2,y2);
             
-            g2d.drawLine(widthSize*i/(nRows+1),0,widthSize*i/(nRows+1),heightSize);
         }
-        for(int j=0;j<=nColumns;j++){
-            g2d.drawLine(0,heightSize*j/(nColumns+1),widthSize,heightSize*j/(nColumns+1));
+        for(int j=0;j<=nColumns+1;j++){
+            x1=offset+newWidth*j/(nColumns+1);
+            y1=offset;
+            x2=offset+newWidth*j/(nColumns+1);
+            y2=offset+newHeight;
+            g2d.drawLine(x1,y1,x2,y2);
         }
-        
-        //g2d.draw(new Line2D.Double(59.2d, 99.8d, 419.1d, 99.8d));
- 
-        //g2d.draw(new Line2D.Float(21.50f, 132.50f, 459.50f, 132.50f));
+        g2d.drawOval(offset+newWidth/2*nColumns,offset+newHeight/2*nRows,newWidth/nColumns,newHeight/nRows);
+        //System.println(offset+newWidth/2*nColumns,offset+newHeight/2*nRows,newWidth/nColumns,newHeight/nRows)
+        //g2d.drawOval(150,150,150,150);
+      
+      //faire un tableau contenant les coordonnées de chaque cases
+      //afficher le tableau relativement à la grille que l'on a créé
+      //essayer de faire bouger un objet pour s'assurer que ça fonctionne bien
  
     }
  
