6a7934bb34367a626765bbe8f720e19b92cfc4c7
Valentin Lucas <neloter59@gmail.com>
2022-10-17 17:20:27 +0200

    Début de la classe Case

---
+++ Case.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+
+public class Case {
+    public int type;
+    private int coordX ;
+    private int coordY;
+
+    public Case (int t, int x, int y){
+        this.coordX = x;
+        this.coordY = y;
+        this.type = t;
+    }
+    
+    public Case(){
+        
+    }
+
+    /*public Array getCoord(){
+
+    }*/
+}
