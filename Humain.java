ef7acec6b8903e94ee621837b428c7762d24274f
Arthur Pourrier <58587962+ArthurIsen@users.noreply.github.com>
2022-10-17 17:10:48 +0200

    Ajout de la classe humain

---
+++ Humain.java
@@ -0,0 +1,28 @@
+public class Humain {
+    private int posX = 0;
+    private int posY = 0;
+    private String[] messages;
+    private int futurX = 0;
+    private int futurY = 0;
+
+    public boolean vivant;
+
+    public Humain(int x, int y) {
+        this.posX = x;
+        this.posY = y;
+        this.vivant = true;
+    }
+
+    private void bouge() {}
+    
+    public void rencontre() {}
+
+    public void changerPosition(int x, int y) {
+        this.posX = x;
+        this.posY = y;
+    }
+
+    public void creve() {
+        this.vivant = false;
+    }
+}
