8b86f47665f27e68c4e7fbd09a3daa61f5f829c4
ClementMar <clement.marchal@student.junia.com>
2022-10-24 14:14:46 +0200

    Ajout des 4 sous classes

---
+++ Nomades_de_l_air.java
@@ -0,0 +1,5 @@
+public class Nomades_de_l_air extends Humain{
+    public Nomades_de_l_air(){
+        super(0, 0);
+    }
+}
---
+++ Royaume_de_la_terre.java
@@ -0,0 +1,5 @@
+public class Royaume_de_la_terre extends Humain{
+    public Royaume_de_la_terre(){
+        super(0, 0);
+    }
+}
