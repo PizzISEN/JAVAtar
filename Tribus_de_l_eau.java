0e4098ea27c4a64febf3f6ae6faa3cda70671e2d
ClementMar <clement.marchal@student.junia.com>
2022-10-24 14:40:49 +0200

    Override des sous classes

--- Tribus_de_l_eau.java
+++ Tribus_de_l_eau.java
@@ -1,5 +1,27 @@
 public class Tribus_de_l_eau extends Humain{
+    private Coord pos = new Coord();
+    public boolean vivant;
+    
     public Tribus_de_l_eau(){
         super(0, 0);
+    }
+    @Override
+    public void rencontre(Humain h){}
+    
+    @Override
+    public void setPos(int x, int y){
+        this.pos.x = x;
+        this.pos.y = y;
+    }
+    
+    @Override
+    public void setFuturePos(int x, int y){
+        this.pos.x = x;
+        this.pos.y = y;
+    }
+    
+    @Override
+    public void mort() {
+        this.vivant = false;
     }
 }
