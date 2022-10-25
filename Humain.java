403aac7b9b43d5d12719899fd310c57094d099e7
Arthur Pourrier <58587962+ArthurIsen@users.noreply.github.com>
2022-10-25 16:33:42 +0200

    Ajout de seDeplacer v1

--- Humain.java
+++ Humain.java
@@ -10,6 +10,7 @@ public abstract class Humain {
     public Humain(int x, int y) {
         this.pos.x = x;
         this.pos.y = y;
+        this.futurePos = this.pos;
         this.vivant = true;
     }
 
@@ -67,6 +68,30 @@ public abstract class Humain {
     public int lancerDeDes() {
         SecureRandom rand = new SecureRandom();
         return rand.nextInt(6);
+    }
+
+    // TODO: Gérer les rencontres et voir si on calcul la futurePos avant ou après s'être déplacé
+    public void seDeplacer(int h, int w) {
+        Coord centre = new Coord(h/2, w/2);
+        int direction = lancerDeDes();
+
+        if(direction == 0 && this.pos.x <= centre.x) {
+            this.futurePos.x -= 1;
+        } else if (direction == 0 && this.pos.x > centre.x) {
+            this.futurePos.x += 1;
+        } else if (direction == 1 && this.pos.y <= centre.y) {
+            this.futurePos.y -= 1;
+        } else if (direction == 1 && this.pos.y > centre.y) {
+            this.futurePos.y += 1;
+        } else if ((direction == 2 || direction == 3) && this.pos.x <= centre.x) {
+            this.futurePos.x += 1;
+        } else if ((direction == 2 || direction == 3) && this.pos.x > centre.x) {
+            this.futurePos.x -= 1;
+        } else if ((direction == 4 || direction == 5) && this.pos.y <= centre.y) {
+            this.futurePos.y += 1;
+        } else if ((direction == 4 || direction == 5) && this.pos.y > centre.y) {
+            this.futurePos.y -= 1;
+        }
     }
 
     public abstract String getEquipe();
