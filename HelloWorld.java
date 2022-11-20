1f70bd32aa6771f59175998f56c0ffd41ec20773
Arthur Pourrier <58587962+ArthurIsen@users.noreply.github.com>
2022-11-20 18:02:52 +0100

    Delete HelloWorld
    Co-authored-by: Valentin Lucas <PizzISEN@users.noreply.github.com>

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int[] s= {10,10};
-        System.out.println("Constructeur");
-        Carte c = new Carte(s);
-        c.matrice(new Coord(0,6),new Coord(0,3),"A");
-    }
-}
